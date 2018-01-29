2c055720a34b0622da4729922ff40d7a29f50651
Eden Mugg <brillianteden@hotmail.com>
2018-01-29 22:03:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,8 +25,10 @@ function doWhileLoop(array) {
   
   do {
   array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
+  } 
+  
+  while (array.length > 0 && maybeTrue()) {
 
   return array
-}}
+  }
 }
